103038b593ed11d0897e5281cdcf91cb7a1ab8d2
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-08-14 09:43:50 +0900

    m

--- components/env.ts
+++ components/env.ts
@@ -1,2 +1,2 @@
-export const PIAZZA_DOMAIN = "https://piazza.run" // prod
-// export const WAITLIST_SERVER_URL = "http://localhost:4416" // dev
+// export const PIAZZA_DOMAIN = "https://piazza.run" // prod
+export const PIAZZA_DOMAIN = "http://localhost:4416" // dev
